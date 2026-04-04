48872d57db39a07788b048d6eb5ca90137779268
BilalNasir5 <pakfighterf17@gmail.com>
2026-04-04 16:30:15 +0500

    Update main.js

--- main.js
+++ main.js
@@ -72,8 +72,9 @@ window.closePlayer = closePlayer;
 // Dynamic games from API
 async function getDynamicGames(){
   try {
-    const res = await fetch("https://www.freetogame.com/api/games");
+    const res = await fetch("https://api.allorigins.win/raw?url=https://www.freetogame.com/api/games");
     const games = await res.json();
+
     return games.slice(0, 20).map(g => ({
       title: g.title,
       category: g.genre.toLowerCase(),
